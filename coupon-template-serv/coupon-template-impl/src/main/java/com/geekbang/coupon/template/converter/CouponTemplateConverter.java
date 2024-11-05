aa5cd60a5918410aecf15d8902348d200231edc0
Jeffreyhh <jeffreyhh@JeffreyhhdeMacBook-Pro.local>
2024-11-05 21:05:29 +0800

    temp fix

--- coupon-template-serv/coupon-template-impl/src/main/java/com/geekbang/coupon/template/converter/CouponTemplateConverter.java
+++ coupon-template-serv/coupon-template-impl/src/main/java/com/geekbang/coupon/template/converter/CouponTemplateConverter.java
@@ -3,6 +3,7 @@ package com.geekbang.coupon.template.converter;
 import com.geekbang.coupon.template.api.beans.CouponTemplateInfo;
 import com.geekbang.coupon.template.dao.entity.CouponTemplate;
 
+
 public class CouponTemplateConverter {
     public static CouponTemplateInfo convertToTemplateInfo(CouponTemplate template) {
         return CouponTemplateInfo.builder()
